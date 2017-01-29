94afc24a774b4442d3da1c5928abf0eed57f0c88
Marek Dano <mk.dano@gmail.com>
2017-01-29 00:10:53 +0000

    Change the medium size of location images when they upload using paperclip. Add more images of rooms into the assets/images.

--- app/models/location_image.rb
+++ app/models/location_image.rb
@@ -14,7 +14,7 @@ class LocationImage < ApplicationRecord
     styles: {
       thumb: '100x100>',
       square: '200x200#',
-      medium: '300x300>'
+      medium: '800x800>'
     },
     :default_url => "/images/:style/missing.png"
 
